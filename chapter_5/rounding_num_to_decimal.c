6e4f05d96b3fe0b9fab7f8950a02d4ec869c359b
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-02-27 21:23:45 -0600

    Update rounding_num_to_decimal.c

--- chapter_5/rounding_num_to_decimal.c
+++ chapter_5/rounding_num_to_decimal.c
@@ -5,9 +5,18 @@ float round_num(float num){
     int dec_place = 0;
     printf("Enter the decimal place you want to round off to: ");
     scanf("%d", &dec_place);
+    if(dec_place % 10 == 0){
+        
     float y = floor((num * dec_place) + 0.5) / (dec_place);
-    
     return y;
+    
+    }
+    else{
+        
+        printf("Enter a legit number.");
+        return 0;
+    }
+    
 }
 
 int main(){
