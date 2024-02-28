28c3bb20aba87f2583cb7869d434e60992485d24
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-02-27 21:19:27 -0600

    Create rounding_num_to_decimal.c
    This program rounds off the number based on user input to the wanted decimal place based on user input.

---
+++ chapter_5/rounding_num_to_decimal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+float round_num(float num){
+    int dec_place = 0;
+    printf("Enter the decimal place you want to round off to: ");
+    scanf("%d", &dec_place);
+    float y = floor((num * dec_place) + 0.5) / (dec_place);
+    
+    return y;
+}
+
+int main(){
+    float num = 0;
+    printf("Enter the number you want to round off: ");
+    scanf("%f", &num);
+    
+    printf("The rounded off number is %f", round_num(num));
+    
+}
