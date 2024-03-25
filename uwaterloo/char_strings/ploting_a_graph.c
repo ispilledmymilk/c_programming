3e31754627161e5e8b9359e43ca93478e2441768
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-03-25 16:52:08 -0600

    Update ploting_a_graph.c

--- uwaterloo/char_strings/ploting_a_graph.c
+++ uwaterloo/char_strings/ploting_a_graph.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
-#define MAX 65
+#define MAX 80 
 
 int fn(int t){
-    return (t + t);             //these are the plot points
+    return (t*t + 1);
 }
 
 int main(){
-    char plot[MAX + 2];         //this is an array
-    int i, t, funval;
     
-    for(i = 0; i<= MAX; i+= 5) 
-        printf("%5d",i);        //this is to print the numbers - x axis
+    char plot[MAX + 2];
+    int funval;
+    
+    for(int i = 0; i <= MAX; i += 5){
+        printf("%5d", i);       //this is to print the numbers on the x axis
+    }
     printf("\n");
     
-    for(i = 0; i <=MAX; i += 5)
-        printf("    |");        //this is to print the | for every interval - x axis
+    for(int i = 0; i <= MAX; i += 5){
+        printf("    |");        //this is to print the x axis
+    }
     printf("\n");
     
-    for(i = 0; i <= MAX; i++)
-        plot[i] = ' ';          //idk what this is doing
+    for(int i = 0; i < MAX + 1; ++i){
+        plot[i] = ' ';          //this is to set the entire array of each line to be space
+    }
     
-    for(t = 0; t <= 10; ++t){
-        funval = fn(t);         //store the plot point in funval
-        plot[funval] = '*';     // for the point  of the function, you use the array to plot at the right position
-        plot[funval + 1] = '\0';    // this prints a space next to the * 
-        printf("t=%2d%s\n", t, plot);   //this is to print the y axis
-        plot[funval] = ' ';         //this is to print a space before the *
-        plot[funval + 1] = ' ';     //this is to print a space before the *
+    for(int t = 0; t <= 10; ++t){
+        funval = fn(t);     //this is to set the value returned by the function
+        plot[funval] = '*';         //at that element we will print a *
+        plot[funval + 1] = '\0';    //at the next element we print a null char
+        printf("t=%2d%s\n", t, plot);   //this is to print the x axis
+        plot[funval] = ' ';         //this is to reset the value of the array at the position funval and funval + 1
+        plot[funval + 1] = ' ';
+        
+        
+        
     }
 }
