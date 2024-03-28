9d9822a9f8a6be0ae23965b741a1845782658d63
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-03-28 11:43:08 -0600

    Update selection_sort.c

--- uwaterloo/searching_sorting_algorithms/selection_sort.c
+++ uwaterloo/searching_sorting_algorithms/selection_sort.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-void selection_sort(int a[], int n){
-    for (int i = 0; i < n; ++i){
+void selection_sort(int a[], int n){        
+    for (int i = 0; i < n; ++i){//for every position in the array we set min to be that postion in the array
         int min = i;
-        for(int j = i+ 1; j < n; ++j){
+        // then for all the positions greater than i
+        for(int j = i + 1; j < n; ++j){
+            //if the element on the right is lesser than the one on the left, we set min to be that position
             if(a[j] < a[min])
                 min = j;
-        }
+        }//then we do the swap of the elements at i and j
         int temp = a[min];
         a[min] = a[i];
         a[i] = temp;
