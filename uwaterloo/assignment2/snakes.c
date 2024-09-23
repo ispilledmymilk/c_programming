59c1fe0c275f7239b57e332f15d1f0f3817d01b8
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-09-22 22:43:57 -0400

    Update snakes.c

--- uwaterloo/assignment2/snakes.c
+++ uwaterloo/assignment2/snakes.c
@@ -1,39 +1,53 @@
 #include <stdio.h>
 
-int main(){
-    int n = 0;
-    //printf("Enter the value of n: ");
-    scanf("%d", &n);
+int main() {
+    int n;
     
-    if(n < 5){
-        printf("Sorry. Exiting the program.");
+    // Read the value of n, ensure n is at least 5
+   // printf("Enter the value of n (at least 5): ");
+    scanf("%d", &n);
+
+    if (n < 5) {
+        printf("Invalid input, n should be at least 5.\n");
         return 1;
     }
-    int head = 3 ;// since the program says so, will try changing to 4
-    int length = 2 ;//the program req mentions this
-    int lastbaiteaten = 0;
-    
-    while(head < n){
-        for ( int i = 0; i < n; i++){
-            if (i == head){
-                printf("H");
+
+    // Snake's initial configuration
+    int head = 4;   // Snake's head starts at position 3
+    int length = 2; // Snake's body length starts at 2
+    int baitEaten;  // Flag to track if bait was eaten at a position
+
+    // The snake moves one position to the right in each loop
+    while (head < n) {
+        baitEaten = 0; // Reset baitEaten for each move
+
+        // Print the current state of the line
+        for (int i = 0; i < n; i++) {
+            if (i == head) {
+                printf("H"); // Snake's head
+            } else if (i >= head - length && i < head) {
+                printf("X"); // Snake's body
+            } else if (i % 2 == 1 && i > length) {
+                printf("."); // Bait at odd positions to the right of the body
+            } else {
+                printf("_"); // Empty space
             }
-            else if(i >= head - length  && i <= head){
-                printf("X");
-            }
-            else if ( i % 2 == 1 && i >= head - length + 1 && i >= lastbaiteaten + 1){
-                printf(".");
-            }
-            else{
-                printf("_");
-            }
-            
         }
         printf("\n");
-        if ( head % 2 == 1 && head > lastbaiteaten + 1 && head > 3){
-            length++;
-            lastbaiteaten == head;
+
+        // Check if the snake's head is on a bait
+        if (head % 2 == 1) {
+            baitEaten = 1; // Bait eaten at this position
         }
+
+        // Move the snake's head to the right
         head++;
+
+        // If bait was eaten, increase the snake's length
+        if (baitEaten) {
+            length++;
+        }
     }
+
+    return 0;
 }
