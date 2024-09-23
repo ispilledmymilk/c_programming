a54207abf7dc481adeb005b9150ebe917478c8c3
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-09-23 09:06:17 -0400

    Create snake_final.c

---
+++ uwaterloo/assignment2/snake_final.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    int n = 0;
+    //printf("Enter the value of n: ");
+    scanf("%d", &n);
+    
+    if(n < 5){
+        printf("Sorry. Exiting the program.");
+        return 1;
+    }
+    int head = 3 ;// since the program says so, will try changing to 4
+    int length = 2 ;//the program req mentions this
+    int lastbaiteaten = 0;
+    int i;
+    
+    while(head < n){
+        for ( i = 0; i < n; i++){
+            if (i == head){
+                printf("H");
+            }
+            else if(i >= head - length  && i <= head){
+                printf("X");
+            }
+            else if ( i % 2 == 1 && i >= head - length + 1 && i >= lastbaiteaten + 1){
+                printf(".");
+            }
+            else{
+                printf("_");
+            }
+            
+        }
+        printf("\n");
+        if ( head % 2 == 0 && head >= lastbaiteaten + 1){
+            length++;
+            lastbaiteaten == head;
+        }
+        head++;
+    }
+}
