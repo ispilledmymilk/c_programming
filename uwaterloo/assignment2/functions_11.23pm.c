34b414af47dbbd0fe9a251d2f1a10d188895192f
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-09-26 23:28:59 -0400

    Update functions_11.23pm.c

--- uwaterloo/assignment2/functions_11.23pm.c
+++ uwaterloo/assignment2/functions_11.23pm.c
@@ -2,6 +2,60 @@
 #include <assert.h>
 #include "functions.h"
 
+void square(int w) {
+   int count = 1;  // Start counting from 1
+
+
+   // Outer loop for each row
+   for (int i = 0; i < w; i++) {
+       // Inner loop for each column in the row
+       for (int j = 0; j < w; j++) {
+           printf("%d ", count);
+           count++;  // Move to the next number
+       }
+       printf("\n");  // Move to the next line after each row
+   }
+}
+
+
+void spiral(int w){
+  
+   int i, j ,k, l;
+   int m = 0;
+   int x = 1;
+   int incre1 = 0;
+   int decre1 = 0;
+   int incre2 = 0;
+   int decre2 = 0;
+  
+   while(x <= w/2){
+       for(i = m + 1; i < w + incre1; i++){
+           printf("%d ", i);
+       }
+       for(j = i; j < w*w - decre1; j+=w){
+           printf("%d ", j);
+       }
+      
+       for(k = j; k  > w*w - w - incre2; k--){
+           printf("%d ", k);
+       }
+       incre2 += (w - 1);
+       for(l = k - w + 1; l > w + decre2; l= l - w){
+           printf("%d ", l);
+           m = l;
+       }
+       x++;
+       incre1 += w - 1;
+       decre1 += w + 1;
+       decre2 += w + 1;
+     
+   }
+   if (w % 2 != 0) {
+       printf("%d ", w * w / 2 + 1);
+   }
+   printf("\n");
+}
+
 
 void rotation(int w){
     int n = 0; // this is to find the number of layers
