09d79ff89af65342ada8ed30c704cf5cdaa36b5a
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-09-26 20:13:08 -0400

    Update fun.c

--- uwaterloo/assignment2/fun.c
+++ uwaterloo/assignment2/fun.c
@@ -1,66 +1,144 @@
 #include <stdio.h>
-#include <assert.h>
-#include "fun.h"
+//#include "functions.h"
 
-int isSophieGermainPrime(int p){
-    //dont know why it is printing twice
-    int div = 2;
-    int x = 2 * p + 1;
-    
-    if(p <= 1){
-       return 0;
+
+void square(int w) {
+   int count = 1;  // Start counting from 1
+
+
+   // Outer loop for each row
+   for (int i = 0; i < w; i++) {
+       // Inner loop for each column in the row
+       for (int j = 0; j < w; j++) {
+           printf("%d ", count);
+           count++;  // Move to the next number
+       }
+       printf("\n");  // Move to the next line after each row
+   }
+}
+
+
+void spiral(int w){
+  
+   int i, j ,k, l;
+   int m = 0;
+   int x = 1;
+   int incre1 = 0;
+   int decre1 = 0;
+   int incre2 = 0;
+   int decre2 = 0;
+  
+   while(x <= w/2){
+       for(i = m + 1; i < w + incre1; i++){
+           printf("%d ", i);
+       }
+       for(j = i; j < w*w - decre1; j+=w){
+           printf("%d ", j);
+       }
+      
+       for(k = j; k  > w*w - w - incre2; k--){
+           printf("%d ", k);
+       }
+       incre2 += (w - 1);
+       for(l = k - w + 1; l > w + decre2; l= l - w){
+           printf("%d ", l);
+           m = l;
+       }
+       x++;
+       incre1 += w - 1;
+       decre1 += w + 1;
+       decre2 += w + 1;
+     
+   }
+   if (w % 2 != 0) {
+       printf("%d ", w * w / 2 + 1);
+   }
+   printf("\n");
+}
+
+
+void rotation(int w){
+ /*  int i = 1, j = 0, x;
+    int incre = 0;
+    int decre = 0;
+    int num = 0;
+    if(w % 2 == 0){
+        num = w/2;
     }
     else{
-        while ( div * div <= p){
-            if (p % div == 0 || x % div == 0){
-                return 0;
-            }
-            else
-                return 1;
-            }
-            div++;
+        num = w/2 + 1;
+    }
+    for( j = 1; j < num; j++){
+        for(i = 1; i < w; i++){
+            x = i;
+            printf("%d ", x);
+            printf("%d ", x = x + w - 1 + incre);
+            printf("%d ", x = x + (w*w) - w - decre);
+            printf("%d ", x = x - w + 1 - incre);
+
+            incre += w - 1;
+            decre += w + 1;
+            
+    }
+       if (w % 2 != 0) {
+       printf("%d ", w * w / 2 + 1);
+       
+   }
+   printf("\n");
+   
+      
+}
+}
+
+*/
+int topleft = 1;
+int topright = w;
+int bottomleft = (w - 1) * w + 1;
+int bottomright = w*w;
+
+int counter = w - 1;
+
+
+
+    while (topleft > 0 && topleft != topright){
+        int i = 0;
+        while(i < counter){
+            printf("%d ", topleft + i);
+            printf("%d ", topright + (i*w));
+            printf("%d ", bottomright - i);
+            printf("%d ", bottomleft - (w*i));
+            i++;
+        }
+        
+        topleft = topleft + w + 1;
+        bottomleft = bottomleft - w + 1;
+        topright = topright + w - 1;
+        bottomright = bottomright - w - 1;
+        counter -= 2;
+        
+        if(w % 2 != 0){
+            printf("%d ", topright);
         }
     }
+    printf("\n");
 
-int base2nat(int bs, int num){
-    int decimal_value = 0;  // To store the resulting decimal value
-    int power_of_base = 1;  // Start with bs^0 which is 1
-    
-    // Loop until num becomes 0
-    while (num > 0) {
-        int digit = num % 10;          // Extract the last digit
-        decimal_value += digit * power_of_base;  // Convert the digit to its decimal equivalent
-        num /= 10;                     // Remove the last digit from num
-        power_of_base *= bs;           // Increase the power of the base for the next iteration
-    }
-    
-    return decimal_value;
-    
 }
 
-int nat2base(int base, int num){
-    // Handle the case when the number is zero
-    if (num == 0) {
-        return 0;
-    }
 
-    int result = 0;   // Variable to store the final result
-    int place = 1;    // Keeps track of the place value (units, tens, hundreds, etc.)
-    int remainder;
 
-    // Process the number by repeatedly dividing by base
-    while (num > 0) {
-        remainder = num % base;          // Get the current digit
-        result = result + remainder * place;  // Add the digit to the result at the right place
-        place *= 10;                     // Move to the next place (next digit)
-        num /= base;                     // Update the number by dividing by base
-    }
-
-    return result;
-}
 
 /*int main(){
-    base2nat(7, 1);
-    printf("\n");
-    nat2base(5, 1659);
+   int a = 0;
+   scanf("%d", &a);
+   square(a);
+   printf("\n");
+   spiral(a);
+   printf("\n");
+   rotation(a);
+   printf("\n");
 }*/
+
+
+
+
+
