ab773e0a222be6996ea4615b60d1157ee03ee32e
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-09-30 14:04:09 -0400

    Update narcissist_2.c

--- uwaterloo/assignment_3/narcissist_2.c
+++ uwaterloo/assignment_3/narcissist_2.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 
 int isnarc(int n){
-    int x = n%10;
-    int number;// here number is 
-    int l;
-    if (n % 10 == n){
-        
-        printf("%d %dx ",n, number);
-        number += n*n*n;
+    int x = 0;
+  
+    if (n == 0){
+        return 0;
     }
     else{
-        
-        printf("%d ", x);
-        number = (x*x*x);
-        printf("%d\n", number);
-        l += number;
+        x = n%10;
         n /= 10;
-        isnarc(n);
+
+        return ((x*x*x) + isnarc(n));
         
     }
-    printf("\n%d", l);
-    
-    
+  
+}
+
+bool narcissist(int n){
+    if(isnarc(n) == n){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 int main(){
-     isnarc(153);
+    assert(narcissist(9));
+    assert(narcissist(153));
+    assert(narcissist(370));
+    assert(narcissist(92727));
+    assert(narcissist(548834));
+    assert(!narcissist(10));
+    assert(!narcissist(92));
+    assert(!narcissist(1535));
+    assert(!narcissist(1234));
+    assert(!narcissist(92726));
 }
